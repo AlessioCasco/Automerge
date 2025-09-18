61f2a64b1d168f3ff471d872569b834eb9786033
Matteo Madeddu <matteo.madeddu@truelayer.com>
2025-09-18 11:42:21 +0200

    Github topic limitations are no sense

--- src/utils.py
+++ src/utils.py
@@ -29,7 +29,7 @@ LABEL_AUTOMERGE_DISMISSED = "automerge_dismissed"
 LABEL_AUTOMERGE_CONFLICT = "automerge_conflict"
 
 # AI Configuration
-AI_DISABLE_KEYWORD = "automerge_ai_disabled"
+AI_DISABLE_KEYWORD = "automerge-ai-disable"
 
 # Comments
 COMMENT_ATLANTIS_PLAN = "atlantis plan"
